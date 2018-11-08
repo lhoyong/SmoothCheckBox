e2792dc39cd9fafe16600fe61d73ab9f36c92850
leehoyong <lee199402@gmail.com>
2018-11-08 16:59:56 +0900

    # Migrate AndroidX
    * update gradle and support lib

--- sample/src/main/java/cn/refactor/smoothcheckbox/SampleActivity.java
+++ sample/src/main/java/cn/refactor/smoothcheckbox/SampleActivity.java
@@ -1,7 +1,7 @@
 package cn.refactor.smoothcheckbox;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 
 import cn.refactor.library.SmoothCheckBox;
--- sample/src/main/java/cn/refactor/smoothcheckbox/SampleListViewActivity.java
+++ sample/src/main/java/cn/refactor/smoothcheckbox/SampleListViewActivity.java
@@ -1,7 +1,7 @@
 package cn.refactor.smoothcheckbox;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
